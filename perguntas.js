6a6fb16500d5b7703b286eb4219a3ba19e311c30
pedro12972 <pedro.graciano.lopes@escola.pr.gov.br>
2025-07-02 10:33:41 -0300

    final

---
+++ perguntas.js
@@ -0,0 +1,32 @@
+criaCartao(
+    'Copa do Mundo ',
+    'Quantas vezes o Brasil foi Campeao da Copa?',
+    '5 vezes'
+)
+
+criaCartao(
+    'Copa do Mundo',
+    'Qual pais sediou a Copa do Mundo de 2018?',
+    'Russia'
+)
+
+criaCartao(
+    'Copa do Mundo',
+    'Quantas seleções ja conquistaram a Copa do Mundo da Fifa até 2022?',
+    '8,Brasil,Alemanha, Itália,Argentina,Uruguai,França,Inglaterra, Espanha'
+)
+
+criaCartao(
+    'Copa do Mundo',
+    'Em que ano foi realizado a primeira edição da Copa do Mundo?',
+    '1930'
+)
+criaCartao(
+    'Copa do Mundo',
+    'Em que pais foi a Copa de 2014?',
+    'Foi no Brasil'
+)
+criaCartao(
+    'Copa do mundo',
+    'Qual foi a primeira vez que a Copa do Mundo sediou no Brasil?',
+    '1950'
